3a94df70f5d615a7385e4ae64fb891421a56328c
Angelo Resplandes <angeloodrigues@gmail.com>
2025-03-03 12:52:57 -0300

    fix: server.listen PORT in the server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,8 @@ server.use("/", router)
 server.use(NotFoundRequest)
 server.use(errorHandler)
 
-server.listen(3000, () => {
-    console.log("Servidor rodando no link http://localhost:3000/")
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
